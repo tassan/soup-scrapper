f176fdd2cde6ec0f3a134a65f410096f926d9949
Flavio Tassan dos Santos Filho <flavio.tassan>
2018-12-19 16:51:22 -0200

    scrapper is done; and write a csv file

--- scrapper.py
+++ scrapper.py
@@ -7,8 +7,32 @@ my_url = 'https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=77
 uClient = uReq(my_url)
 page_html = uClient.read()
 
+# html parsing
 page_soup = soup(page_html, "html.parser")
 
+# grabs each product
 containers = page_soup.find_all("div", {"class":"item-container"})
 
+filename = "graphic_cards.csv"
+f = open(filename, "w")
+
+headers = "brand, product_name, price"
+
+f.write(headers)
+
+# loop through products and grab the gpu name
+for container in containers:
+    brand_container = container.find("a", {"class":"item-brand"})
+    brand = brand_container.img["title"]
+    product_name = container.a.img["title"]
+    price_container = container.find("li", {"class":"price-current"})
+    price = ''.join([price_container.strong.string, price_container.sup.string])
+    
+    print("brand: " + brand)
+    print("product_name: " + product_name)
+    print("price: " + price)
+
+    f.write(brand + ";" + product_name.replace(",", "|") + ";" + price + "\n")
+
+f.close
 uClient.close()
